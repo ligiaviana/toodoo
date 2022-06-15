e471920c05cbe49249c5f310abe0ce0c99fcca03
joao rosa <joao.rosa@gec.inatel.br>
2022-06-15 08:46:55 -0300

    Toodoo

---
+++ q11.ts
@@ -0,0 +1,9 @@
+function multiplicacao(num1, num2) {
+  let resultado = num1 * num2
+  console.log(resultado)
+}
+multiplica(4, 7)
+multiplica(5, 6)
+multiplica(3, 3)
+
+// Não exibir nada pois o nome da função é diferente
